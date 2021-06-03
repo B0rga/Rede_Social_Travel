617b1011b25f329dba7a6887c19632355ffc3975
Pedro Borges <pedro.borges.jesus@gmail.com>
2021-06-03 13:42:24 -0300

    ReactiveForms adicionado a tela de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule  } from '@angular/forms';
+import { LoginPage } from './login.page';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
-import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     LoginPageRoutingModule
   ],
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +12,33 @@ export class LoginPage implements OnInit {
   showPassword = false;
   passwordToggleIcon = 'eye-outline';
 
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get senha(){
+    return this.loginForm.get('senha');
+  }
+
+  public errorMessages ={
+    email: [
+      {type: 'required', message: 'Email é obrigatório'},
+      {type: 'pattern', message: 'Por favor insira um email válido'}
+    ],
+    senha: [
+      {type: 'required', message: 'Senha é obrigatória'},
+      {type: 'minlength', message: 'Senha não poder ter menos que 6 caracteres'}
+    ]
+  }
+
+  loginForm = this.FormBuilder.group({
+    email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+    senha:  ['', [Validators.required, Validators.minLength(6)]]
+  })
+
   constructor(
     private router: Router,
+    private FormBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
@@ -37,6 +62,14 @@ export class LoginPage implements OnInit {
 
   btnRecuperarSenha(){
     this.router.navigate(['esqueceu-senha']);
+  }
+
+  public submit(){
+    if(this.loginForm.valid){
+    console.log(this.loginForm.value);
+    }else {
+      alert("preencha os campos");
+    }
   }
 
 }
