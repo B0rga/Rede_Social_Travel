9640e1e7eec00e0102f2c6743294c89170a2c702
Pedro Borges <pedro.borges.jesus@gmail.com>
2021-09-30 13:15:23 -0300

    pops adicionados ao post-completo

--- src/app/post-completo/post-completo.page.ts
+++ src/app/post-completo/post-completo.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { EstrelasComponent } from './../components/estrelas/estrelas.component';
+import { PopoverComponent } from './../components/popover/popover.component';
 
 @Component({
   selector: 'app-post-completo',
@@ -11,11 +12,21 @@ export class PostCompletoPage implements OnInit {
 
   constructor(public popoverController: PopoverController) { }
 
-  async openPopover(ev: any) {
+  async openStars(ev: any) {
     event.stopPropagation();
     const popover = await this.popoverController.create({
       component: EstrelasComponent,
       cssClass: 'estrelaPopover',
+      event: ev,
+    });
+    return await popover.present();
+  }
+
+  async openPopover(ev: any) {
+    event.stopPropagation();
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      cssClass: 'customPopover',
       event: ev,
     });
     return await popover.present();
